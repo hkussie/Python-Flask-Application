0210c23c0ff863052d9ee107d3966f5fcc231fc6
hkussie <hkussie@gmail.com>
2017-12-20 17:39:33 -0500

    Add Route for article.html

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 app.debug = True
 
+Articles = Articles()
+
 @app.route('/')
 def index():
     return render_template('index.html')
-    # return 'Hello There'
-    
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:title>/')
+def article(title):
+    return render_template('article.html', title=title)
+
 if __name__ == '__main__':
     app.run()
