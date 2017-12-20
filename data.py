3732f396b74a162ba72feacb30aea45530b3fd9b
hkussie <hkussie@gmail.com>
2017-12-20 04:17:47 -0500

    Add Mock Data to Project

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+        {
+           'id': 1,
+           'title': 'Article One',
+           'body': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+           'author': 'Harrison Kussie',
+           'publication_date': '12-19-2017'
+        },
+
+        {
+           'id': 1,
+           'title': 'Article One',
+           'body': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+           'author': 'Harrison Kussie',
+           'publication_date': '12-19-2017'
+        },
+
+        {
+           'id': 1,
+           'title': 'Article One',
+           'body': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+           'author': 'Harrison Kussie',
+           'publication_date': '12-19-2017'
+        }
+    ]
+    return articles
